7cd165ae1342c98c8b737c4948c7709bc9fdcca3
Nicolas R <atoomic@cpan.org>
2025-05-01 18:24:16 +0200

    Update font-awesome to version 4.7.0

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -4,13 +4,9 @@ echo "Starting the application..."
 echo "Environment: $APP_ENV"
 echo "Host Port: $HOST_PORT"
 echo "Plackup Options: $PLACKUP_ARGS"
-
+echo "git binary: $(which git) - $(git --version)"
 echo "========================="
-which git
-git --version
+echo "Access the application at http://localhost:${HOST_PORT}"
 echo "========================="
-
-pwd
-ls -l /metacpan-grep-front-end
 
 plackup -p 3000 ${PLACKUP_ARGS} bin/app.psgi
